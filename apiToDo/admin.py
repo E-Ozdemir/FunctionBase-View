b75b296c71ff7ab627e0e080b2bedf6e96c11d3a
E-Ozdemir <emreclarusway@gmail.com>
2021-11-08 19:19:20 +0100

    admin pannel added

--- apiToDo/admin.py
+++ apiToDo/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import ToDo
 # Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = [
+        "task",
+        "description",
+        "priority",
+        "done",
+        "updateDate",
+        
+    ]
+    
+    
+    
+    
+    
+admin.site.register(ToDo,TodoAdmin)
