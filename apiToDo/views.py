6929d10e1b5eb66867f1510f8a76fb05d96dc68c
E-Ozdemir <emreclarusway@gmail.com>
2021-11-08 20:18:38 +0100

    api view get and post

--- apiToDo/views.py
+++ apiToDo/views.py
@@ -6,6 +6,7 @@ from .models import ToDo
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 
 # Create your views here.
@@ -20,4 +21,26 @@ def todoList(request):
      
      serializer =TodoSerializer(queryset, many = True)
      return Response(serializer.data)
+     
+     
+@api_view(['POST'])
+def todoListCreate(request):
+     serializer = TodoSerializer(data = request.data)
+     if serializer.is_valid():
+          serializer.save()
+     return Response(serializer.data)
+
+@api_view(['GET','POST'])
+def toDo_list(request):
+     
+     if request.method == 'GET':
+          queryset = ToDo.objects.all()
+          serializer =TodoSerializer(queryset, many = True)
+          return Response(serializer.data)
+     elif request.method == 'POST':
+          serializer = TodoSerializer(data = request.data)
+          if serializer.is_valid():
+               serializer.save()
+               return Response(serializer.data,status=status.HTTP_201_CREATED)
+          return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
      
